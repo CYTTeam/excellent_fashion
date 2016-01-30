85738f1afb92e726c32b208e23d285fadaf06d77
Liz606 <1105394023@qq.com>
2016-01-30 22:39:50 +0800

    page3/images

--- src/subbrand.js
+++ src/subbrand.js
@@ -2,8 +2,14 @@ require('./js/base.js');
 require('./css/subbrand.css');
 $(".weixinIco").mouseover(function(){
 	console.log("mouseover");
-  $(".weixinIco2").addClass('Liz-block').addClass('Liz-none').removeClass('Liz-none');
+  $(".weixinIco2")
+  .addClass('Liz-block')
+  .addClass('Liz-none')
+  .removeClass('Liz-none');
   $(".weixinIco2").mouseleave(function(){
-  $(".weixinIco2").addClass('Liz-block').addClass('Liz-none').removeClass('Liz-block');
+  $(".weixinIco2")
+  .addClass('Liz-block')
+  .addClass('Liz-none')
+  .removeClass('Liz-block');
 });
 });
